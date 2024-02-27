7f012fe51821e4b79100de2c2d4465da66851b23
Gonzalo Maldonado <nahuel.maldonado.gonzalo@gmail.com>
2024-02-27 00:21:41 -0300

    feat: añadiendo authenticate para login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,37 +1,23 @@
 "use client"
 
-import { useState } from "react";
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
-import { redirect } from "next/navigation"
 import Link from "next/link";
 import Image from "next/image";
-import {
-    AtSymbolIcon,
-    KeyIcon,
-    ExclamationCircleIcon,
-  } from '@heroicons/react/24/outline';
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 //import { toast } from 'react-hot-toast'
 import Logo from '@/public/lion7.png';
 
 
-
 const Login = () => {
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const { pending } = useFormStatus();
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
-
-
+  const { pending } = useFormStatus();
 
   /* const loginMutation = useMutation({
     mutationFn:() => loginReq(username, password),
     onSuccess:(res) => {
-      setToken(res.access, res.refresh);
       toast.success("Login successfully");
-      navigate("/")
     },
     onError: (error) => {
       toast.error("There was an error, please try to login again")
@@ -39,7 +25,6 @@ const Login = () => {
     }
   }) */
 
-  //if (loginMutation.isLoading) return <Loader />
   //if (isAuth) return (<Navigate to="/" />)
 
   return (
@@ -57,18 +42,19 @@ const Login = () => {
           <form action={dispatch} className="p-3 space-y-2 md:space-y-4">
 
               <label htmlFor="username" className="block text-sm lg:text-base font-medium">UserName</label>
-              <input type="username" name="username" id="username" 
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+              <input type="username" name="username" id="username"
+                placeholder="Enter your username"
                 required
-                className="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="your username"/>
+                className="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+              />
 
               <label htmlFor="password" className="block text-sm lg:text-base font-medium">Password</label>
               <input type="password" name="password" id="password" 
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 required
-                placeholder="••••••••" className="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"/>
+                placeholder="••••••••"
+                minLength={6}
+                className="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+              />
 
               <button type='submit' className="bg-sky-400 w-full hover:bg-sky-500 p-2 rounded-full text-white font-bold" aria-disabled={pending}>
                   Login
@@ -105,59 +91,3 @@ const Login = () => {
 }
 
 export default Login
-
-/* 'use client';
-import { lusitana } from '@/app/ui/fonts';
-export default function LoginForm() {
-  return (
-    <form className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
-  );
-}
-*/
--- auth.ts
+++ auth.ts
@@ -7,9 +7,9 @@ import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 
 
-async function getUser(email: string): Promise<User | undefined> {
+async function getUser(username: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    const user = await sql<User>`SELECT * FROM users WHERE email=${username}`;
     return user.rows[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
@@ -23,12 +23,12 @@ export const { auth, signIn, signOut } = NextAuth({
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
+          .object({ username: z.string(), password: z.string().min(6) })
           .safeParse(credentials);
  
           if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
+            const { username, password } = parsedCredentials.data;
+            const user = await getUser(username);
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
    
